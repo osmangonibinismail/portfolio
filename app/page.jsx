beca61a00d0cbede9cd79e73fc4ca733385001d9
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-07-03 19:41:38 +0600

    all info updated

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Homepage = () => {
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         {/* text */}
         <div className="text-center xl:text-left order-2 xl:order-none">
-          <span className="text-xl">Front-end Developer</span>
+          <span className="text-xl">Web Developer</span>
           <h1 className="h1">Hello I'm <br /><span className="text-accent">Osman Goni</span></h1>
           <p className="max-w-[500px] mb-9 text-white/80">
             I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
